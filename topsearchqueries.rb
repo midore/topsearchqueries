e5e9836eee3d9032f1b6f68d14072e0077522360
midore <midorex@gmail.com>
2010-02-23 19:07:11 +0900

    get_words1 and get_words2

--- topsearchqueries.rb
+++ topsearchqueries.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/local/ruby19
 # coding: utf-8
 # topsearchqueries.rb
 # 2010-02-20
@@ -21,17 +22,15 @@ class TopSearchQueries
     arg.each{|x| (m = /^-(.*)/.match(x); next) if /^-/.match(x); h[m[1]] = x if m}
     x = /month(.*)/.match(h['t'])
     (xi = x[1].to_i; h['t'] = 'month') if x
-    a_term = {'days'=>"過去7日分", 'week'=>"2 週間前", 'month'=>"#{xi}月"}
-    a_search = {'1'=>"すべての検索", '2'=>"ウェブ検索", '3'=>"携帯端末", '4'=>"ブログ検索"}
+    a_term = {'days'=>"過去7日分", 'week'=>"2 週間前", 'month'=>"#{xi}月", 'all'=>'all'}
     a_domain = {'all'=>"すべての Google ドメイン", 'google'=>'google'}
-    @file = h['f']
-    @term = a_term[h['t']] ||= "過去7日分"
-    @domain = a_domain[h['d']] ||= "すべての Google ドメイン"
-    @search = a_search[h['s']] ||= "すべての検索"
+    a_search = {'1'=>"すべての検索", '2'=>"ウェブ検索", '3'=>"携帯端末", '4'=>"ブログ検索"}
+    @file, @title = h['f'], ""
+    @term = a_term[h['t']] ||= a_term['days']
+    @domain = a_domain[h['d']] ||= a_domain['all']
+    @search = a_search[h['s']] ||= a_search['1']
     (h['n'].nil?) ? @num = 10 : @num = h['n'].to_i
-    @title = ""
-    @words = Hash.new(0)
-    @words2 = Hash.new(0)
+    @words1, @words2 = Hash.new(0), Hash.new(0)
   end
 
   def base
@@ -42,26 +41,26 @@ class TopSearchQueries
       x = line.split(",\"")
       area_str, words_str, qw_str = x[0], x[1], x[2]
       term, domain, search = area_str.split(",")
-      next unless term == @term
+      (next unless term == @term) unless @term == 'all'
       next unless search == @search
       next unless domain.include?(@domain)
       @title << "#{term}, #{domain}, #{search}\n"
-      get_words(words_str)
-      get_q(qw_str)
+      get_words1(words_str)
+      get_words2(qw_str)
     }
-    return output unless @words.empty?
+    return output unless @words1.empty?
     print "Nothing.\n\n"
   end
 
-  def get_words(str)
+  def get_words1(str)
     str.split(/\]/).each{|x|
       ary = x.strip.gsub(/\[/, '').split(",")
       next if ary.size < 2
-      @words[ary.first] += ary.last.strip.to_i
+      @words1[ary.first] += ary.last.strip.to_i
     }
   end
 
-  def get_q(str)
+  def get_words2(str)
     return nil unless str
     str.split(/\]/).each{|x|
       ary = x.strip.gsub(/\[/, '').split(",")
@@ -85,14 +84,14 @@ class TopSearchQueries
   def output
     print hyphen, @title, hyphen
     @words2.each{|k,v| to_s([0, k, v].flatten)} 
-    h = @words.sort_by{|k,v| v}.reverse[0..@num-1]
+    h = @words1.sort_by{|k,v| v}.reverse[0..@num-1]
     h.each_with_index{|x,n| to_s([n+1, x].flatten)}
     output_sum(h)
   end
 
   def output_sum(h)
-    s1, s2, s3 = get_sum(@words.values), get_sum(h.map{|x| x[1]}), h.size
-    a1, a2, a3 = ["1", s3, s2], [@num+1, @words.size, s1-s2], ["Total", s1]
+    s1, s2, s3 = get_sum(@words1.values), get_sum(h.map{|x| x[1]}), h.size
+    a1, a2, a3 = ["1", s3, s2], [@num+1, @words1.size, s1-s2], ["Total", s1]
     print hyphen
     printf "[%-3d\..%3d]\s%d\n" % a1
     (printf "[%-3d\..%3d]\s%d\n" % a2 ) unless a2[2] < 1
